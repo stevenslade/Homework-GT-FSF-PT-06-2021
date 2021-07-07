b4c1623b2b872e2823cf513ca2e0e53b55e1eee1
Ben S <benslinde@gmail.com>
2021-07-07 12:57:15 -0400

    Wrote pseudo code for javascript, created two of four character arrays

--- 03-JavaScript-Password-Generator/assets/js/script.js
+++ 03-JavaScript-Password-Generator/assets/js/script.js
@@ -12,3 +12,31 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//Array of numbers
+var numArray = [0,1,2,3,4,5,6,7,8,9];
+
+//Array of lower case letters
+var lowCaseCharArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
+
+//Array of uppercase letters
+
+//Array of special characters
+
+//Write a function called "generatePassword" I think
+function generatePassword() {
+//Prompts to inquire to user about four types of possible characters
+//For each prompt I need to assemble a passwordCharList
+
+//Then I need to use a prompt to determine the length of the password 
+//between 8 and 128 characters
+
+//Then I make a passwordArray by running a random selector over the passwordCharlist
+//in a loop of the password length
+
+//Then this password needs to get returned to the function call in the form of a 
+//string.  this might require a conversion from array to string
+
+}
+
