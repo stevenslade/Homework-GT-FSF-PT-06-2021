a7cca3c66178852ba35f72deff2feb1bbcb900ad
Ben S <benslinde@gmail.com>
2021-07-30 09:36:29 -0400

    added a five day forecast API fetch, removed all reference code from JS

--- 06-Server-Side-APIs-Weather-Dashboard/assets/js/script.js
+++ 06-Server-Side-APIs-Weather-Dashboard/assets/js/script.js
@@ -1,119 +1,7 @@
-// Reference Content for commands
-
-// var userFormEl = document.querySelector('#user-form');
-// var languageButtonsEl = document.querySelector('#language-buttons');
-// var nameInputEl = document.querySelector('#cityname');
-// var repoContainerEl = document.querySelector('#repos-container');
-// var repoSearchTerm = document.querySelector('#repo-search-term');
-
-// var formSubmitHandler = function (event) {
-//   event.preventDefault();
-
-//   var cityname = nameInputEl.value.trim();
-
-//   if (cityname) {
-//     getUserRepos(cityname);
-
-//     repoContainerEl.textContent = '';
-//     nameInputEl.value = '';
-//   } else {
-//     alert('Please enter a valid City Name');
-//   }
-// };
-
-// var buttonClickHandler = function (event) {
-
-//   var language = event.target.getAttribute('data-language');
-//   if (language) {
-//     getFeaturedRepos(language);
-
-//     repoContainerEl.textContent = '';
-//   }
-// };
-
-// var getUserRepos = function (user) {
-//   var apiUrl = 'https://api.github.com/users/' + user + '/repos';
-
-//   fetch(apiUrl)
-//     .then(function (response) {
-//       if (response.ok) {
-//         console.log(response);
-//         response.json().then(function (data) {
-//           console.log(data);
-//           displayRepos(data, user);
-//         });
-//       } else {
-//         alert('Error: ' + response.statusText);
-//       }
-//     })
-//     .catch(function (error) {
-//       alert('Unable to connect to GitHub');
-//     });
-// };
-
-// var getFeaturedRepos = function (language) {
-//   // What are the query parameters doing here?
-//   // TODO: Write your answer here
-
-//   // 
-
-//   var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
-
-//   fetch(apiUrl).then(function (response) {
-//     if (response.ok) {
-//       response.json().then(function (data) {
-//         displayRepos(data.items, language);
-//       });
-//     } else {
-//       alert('Error: ' + response.statusText);
-//     }
-//   });
-// };
-
-// var displayRepos = function (repos, searchTerm) {
-//   if (repos.length === 0) {
-//     repoContainerEl.textContent = 'No repositories found.';
-//     return;
-//   }
-
-//   repoSearchTerm.textContent = searchTerm;
-
-//   for (var i = 0; i < repos.length; i++) {
-//     var repoName = repos[i].owner.login + '/' + repos[i].name;
-
-//     var repoEl = document.createElement('div');
-//     repoEl.classList = 'list-item flex-row justify-space-between align-center';
-
-//     var titleEl = document.createElement('span');
-//     titleEl.textContent = repoName;
-
-//     repoEl.appendChild(titleEl);
-
-//     var statusEl = document.createElement('span');
-//     statusEl.classList = 'flex-row align-center';
-
-//     if (repos[i].open_issues_count > 0) {
-//       statusEl.innerHTML =
-//         "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-//     } else {
-//       statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-//     }
-
-//     repoEl.appendChild(statusEl);
-
-//     repoContainerEl.appendChild(repoEl);
-//   }
-// };
-
-// userFormEl.addEventListener('submit', formSubmitHandler);
-// //languageButtonsEl.addEventListener('click', buttonClickHandler);
-
-
-
-//Content I added 
-
+//Content to display time 
 var datetime = document.querySelector('#currentDay');
 
+//APIKey for the request to openweather
 var APIKey = "18478b8e8991a96f7973ebca1c12f563";
 
 var date = moment(new Date())
@@ -122,13 +10,45 @@ datetime.textContent = (date.format('dddd, MMMM Do YYYY'));
 //Need the date in MM/DD/YY for the weather card
 var todayDate = (date.format('MM/D/YY'));
 
-// Start of T script
+//APIKey for the request to openweather
+var APIKey = "18478b8e8991a96f7973ebca1c12f563";
 
 var historyEl = document.getElementById("history");
 var searchInputEl = document.getElementById("cityname");
 var searchInputBtn = document.getElementById("searchBtn");
 
 var parsedLocations = [];
+
+//Need an API pull to get the 5 day forecast using the same key and same city
+function getFiveDayApi(city) {
+
+  //need a request Url 
+  var queryFiveDayUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&appid=" + APIKey;
+
+  // This request is for  
+  fetch(queryFiveDayUrl)
+  .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+      // console.log("City: ", data.name);
+      // console.log("Temp: ", data.main.temp);
+      // console.log("Wind: ", data.wind.speed);
+      // console.log("Humidity: ", data.main.humidity);
+      // console.log("UV Index: ", data.wind.speed);
+      // console.log("Description: ", data.weather[0].description);
+
+      // the query is successful I have the values I need to set the today card
+      // you don't need variable to use textContent if you have ids
+      // searchCityDate.textContent = data.name + "   (" + todayDate +")";
+      // searchTemp.textContent =  "Temp (F): " + data.main.temp;
+      // searchWind.textContent = "Wind (mph): " + data.wind.speed;
+      // searchHumidity.textContent = "Humidity (%): " + data.main.humidity;
+      // searchDescription.textContent = "Description: " + data.weather[0].description;    
+    });
+  }
+
 
 // Send an API request to the URL
 
@@ -189,6 +109,7 @@ function displaySavedLocations() {
     parsedLocations.forEach(function (item) {
       var listItem = document.createElement('li');
       var content = `<button data-location ="${item}">${item}</button>`;
+      
       listItem.innerHTML = content;
       historyEl.appendChild(listItem);
   }); 
